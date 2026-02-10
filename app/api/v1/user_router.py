2ccac23ce2a934ff954aa249e19a3c4c2b8d58a2
Steven-Ak <stevenakram2@gmail.com>
2026-02-10 13:21:35 +0200

    experiment: JWT integerating with the login and controlling users

--- app/api/v1/user_router.py
+++ app/api/v1/user_router.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter, Depends
 from typing import List
+from app.models.user_model import User
 
-from app.schemas.user_schema import UserCreate, UserResponse
+from app.api.auth_deps import get_current_user
+from app.schemas.user_schema import UserCreate, UserResponse, UserUpdate
 from app.services.user_service import UserService
 from app.api.service_deps import get_user_service
 
-router = APIRouter(prefix="/users", tags=["Users"])
+router = APIRouter(prefix="/users", tags=["Users"], dependencies=[Depends(get_current_user)])
 
 @router.post("", response_model=UserResponse)
 def create(
@@ -17,28 +19,38 @@ def create(
 @router.get("", response_model=List[UserResponse])
 def read_all(
     service: UserService = Depends(get_user_service),
+    
 ):
     return service.get_users()
 
-@router.get("/{user_id}", response_model=UserResponse)
+@router.get("/", response_model=UserResponse)  ############################# Check
 def read_one(
-    user_id: int,
     service: UserService = Depends(get_user_service),
+    user: User = Depends(get_current_user)
 ):
-    return service.get_user(user_id)
+    return service.get_user(user.id)
 
-@router.put("/{user_id}", response_model=UserResponse)
+@router.put("/", response_model=UserResponse)
 def update(
-    user_id: int,
     user: UserCreate,
     service: UserService = Depends(get_user_service),
+    current_user: User = Depends(get_current_user)
 ):
-    return service.update_user(user_id, user)
+    return service.update_user(current_user.user_id, user, current_user)
 
-@router.delete("/{user_id}")
-def delete(
-    user_id: int,
+@router.patch("/", response_model=UserResponse)
+def patch(
+    user: UserUpdate,
     service: UserService = Depends(get_user_service),
+    current_user: User = Depends(get_current_user)
 ):
-    service.delete_user(user_id)
+    return service.patch_user(current_user.id, user, current_user)
+
+@router.delete("/")
+def delete(
+    service: UserService = Depends(get_user_service),
+    current_user: User = Depends(get_current_user)
+):
+    
+    service.delete_user(current_user.id, current_user)
     return {"message": "User deleted"}
