93725e8c8ee5329e2763ce9470f45a2e38797d27
Steven-Ak <stevenakram2@gmail.com>
2026-02-16 11:32:19 +0200

    refactor: changing import names

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
-from app.api.v1.router import router
+from app.api.routers.router import router
 from app.clients.database_clients import postgres_client, supabase_client, close_all_connections
 
 from app.models import item_model, user_item_model, user_model, chat_model
